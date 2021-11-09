986d904bb25495b509cd5df575dc6c3cf1403adb
alina-timir <93979405+alina-timir@users.noreply.github.com>
2021-11-09 21:22:51 +0200

    Init repo with one file

---
+++ test1.py
@@ -0,0 +1,30 @@
+x=5
+print(x)
+if x<10:
+    print("mai mare")
+if x>10:
+        print("mai mic")
+print("finish")
+
+
+x=10
+while x>5:
+    print(x)
+    x=x-1
+print("gataaaa")
+
+x=5
+print(x**3)
+print(x%3)
+print(x/3)
+
+print(1+2**3/4*5)
+
+
+
+
+
+
+
+
+
